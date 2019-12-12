57827e2ea20f3deb8a84b8b5a05e91fd3bfb95f6
Matthew Harshman <mjharshman66@gmail.com>
2019-12-12 14:32:43 -0800

    finished eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,19 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  
+  cache = {0:1, 1:1, 2:2, 3:4}
+  def cache_fib_inner(n):
+    nonlocal cache
+    if n not in cache:
+      cache[n] = cache_fib_inner(n-1) + cache_fib_inner(n-2) + cache_fib_inner(n-3)
+    return cache[n]
+
+  if n==0:
+    return 1
+  else:
+    return cache_fib_inner(n)
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
